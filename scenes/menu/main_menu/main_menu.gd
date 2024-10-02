593e41a69c222b1b13cf77b1deae9723c7d18b6c
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-10-02 21:21:14 +0200

    feat: main menu WIP

--- scenes/menu/main_menu/main_menu.gd
+++ scenes/menu/main_menu/main_menu.gd
@@ -4,3 +4,7 @@ extends Control
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(game)
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	RenderingServer.global_shader_parameter_set("animated_background", toggled_on)
